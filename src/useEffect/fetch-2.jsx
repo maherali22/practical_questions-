db74cf3d988f5d0e14fb87fee8210b0891096e91
Maher <mahiralimattummal@gmail.com>
2025-01-10 22:37:23 +0530

    update

--- src/useEffect/fetch-2.jsx
+++ src/useEffect/fetch-2.jsx
@@ -1,11 +1,13 @@
 const Data = ({ data }) => {
   return (
     <div>
-      <h1 className="flex justify-center text-3xl">Posts</h1>
+      <h1 style={{textAlign:"center" }}>POSTS</h1>
       {data?.map((item) => (
-        <ul className="list-disc" key={item.id}>
-          <li>{item.title}</li>
-          <p>{item.body}</p>
+        <ul style={{ listStyle: "none" }} key={item.id}>
+          <li style={{ color: "red", textAlign: "center", fontSize: "30px" }}>
+            {item.title}
+          </li>
+          <p style={{ fontFamily: "fantasy" }}>{item.body}</p>
         </ul>
       ))}
     </div>
