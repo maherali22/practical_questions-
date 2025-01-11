44928acd05c82ad714333f0b6e2977f6f8c52ff5
Maher <mahiralimattummal@gmail.com>
2025-01-11 22:59:12 +0530

    update

--- src/practical-1/buttons.jsx
+++ src/practical-1/buttons.jsx
@@ -9,15 +9,36 @@ const Buttons = () => {
   };
   return (
     <div>
-      <button style={{color:"black", background:"white"}} onClick={() => handleColor("black")}>Black</button>
-      <button style={{color:"red", background:"white"}} onClick={() => handleColor("red")}>Red</button>
-      <button style={{color:"orange", background:"white"}} onClick={() => handleColor("orange")}>Orange</button>
-      <button style={{color:"green", background:"white"}} onClick={() => handleColor("green")}>Green</button>
-      <button style={{color:"white", background:"black"}} onClick={() => handleColor("white")}>White</button>
-      <button style={{color:"blue", background:"white"}} onClick={() => handleColor("blue")}>Blue</button>
-      <button style={{color:"yellow", background:"white"}} onClick={() => handleColor("yellow")}>Yellow</button>
-      <button style={{color:"purple", background:"white"}} onClick={() => handleColor("purple")}>Purple</button>
-      <button style={{color:"pink", background:"white"}} onClick={() => handleColor("pink")}>Pink</button>
+      <button
+        style={{ color: "black", background: "white" }}
+        onClick={() => handleColor("black")}
+      >
+        Black
+      </button>
+      <button
+        style={{ color: "red", background: "white" }}
+        onClick={() => handleColor("red")}
+      >
+        Red
+      </button>
+      <button
+        style={{ color: "orange", background: "white" }}
+        onClick={() => handleColor("orange")}
+      >
+        Orange
+      </button>
+      <button
+        style={{ color: "green", background: "white" }}
+        onClick={() => handleColor("green")}
+      >
+        Green
+      </button>
+      <button
+        style={{ color: "white", background: "black" }}
+        onClick={() => handleColor("white")}
+      >
+        White
+      </button>
     </div>
   );
 };
